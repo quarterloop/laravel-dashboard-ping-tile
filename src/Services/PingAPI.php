5f87739a0d6936d7a0e1e8f7a09ed484bc07232a
Gero Wallisch <gero.wallisch@me.com>
2022-06-02 16:39:30 +0200

    Build API Call

--- src/Services/PingAPI.php
+++ src/Services/PingAPI.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace Quarterloop\LoadTimeTile\Services;
+namespace Quarterloop\PingTile\Services;
 
 use Illuminate\Support\Facades\Http;
 
-class LoadTimeAPI
+class PingAPI
 {
-  public static function getLoadTime(string $url, string $key): array
+  public static function getPing(string $url, string $key): array
   {
 
       $response = Http::withHeaders([
         'x-api-key' => $key,
-      ])->post('https://api.geekflare.com/loadtime', [
+      ])->post('https://api.geekflare.com/ping', [
         'url' => $url,
         'locations' => [ "uk", "us", "sg" ],
       ])->json();
