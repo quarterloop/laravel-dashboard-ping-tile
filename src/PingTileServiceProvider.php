a880bd18ce35ba4039d6e107468aff7c4153ec05
Gero Wallisch <gero.wallisch@me.com>
2022-06-02 16:39:22 +0200

    Build ServiceProvider

--- src/PingTileServiceProvider.php
+++ src/PingTileServiceProvider.php
@@ -1,27 +1,27 @@
 <?php
 
-namespace Quarterloop\LoadTimeTile;
+namespace Quarterloop\PingTile;
 
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
-use Quarterloop\LoadTimeTile\Commands\FetchLoadTimeCommand;
+use Quarterloop\PingTile\Commands\FetchPingCommand;
 
-class LoadTimeTileServiceProvider extends ServiceProvider
+class PingTileServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         if ($this->app->runningInConsole()) {
             $this->commands([
-                FetchLoadTimeCommand::class,
+                FetchPingCommand::class,
             ]);
         }
 
         $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/dashboard-load-time-tile'),
-        ], 'dashboard-load-time-views');
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/dashboard-ping-tile'),
+        ], 'dashboard-ping-views');
 
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'dashboard-load-time-tile');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'dashboard-ping-tile');
 
-        Livewire::component('load-time-tile', LoadTimeTileComponent::class);
+        Livewire::component('ping-tile', PingTileComponent::class);
     }
 }
